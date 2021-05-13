2a220530a2200cf1163192be274665cd95fd16f2
Brigita <vaiciulyte.brigita@gmail.com>
2021-05-13 20:22:26 +0300

    commit

--- app/src/main/java/com/example/praktinisdarbas2_fintech2021/MainActivity.java
+++ app/src/main/java/com/example/praktinisdarbas2_fintech2021/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.praktinisdarbas2_fintech2021;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
+import static com.example.praktinisdarbas2_fintech2021.utils.ElementsCalculator.getCharsCount;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner ddSelection;
+    EditText edUserInput;
+    TextView tvOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.ddSelection = (Spinner) findViewById(R.id.ddSelection);
+        this.edUserInput = findViewById(R.id.edUserInput);
+        this.tvOutput = findViewById(R.id.tvOutput);
+
+        ArrayList<String> selectionOptionsList = new ArrayList<>();
+        selectionOptionsList.add("Chars");
+        selectionOptionsList.add("Words");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<> (this, android.R.layout.simple_spinner_item, selectionOptionsList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.ddSelection.setAdapter(arrayAdapter);
     }
+
+    public void btnCalculateOnClick(View view) {
+
+        this.ddSelection = (Spinner) findViewById(R.id.ddSelection);
+        this.edUserInput = findViewById(R.id.edUserInput);
+        this.tvOutput = findViewById(R.id.tvOutput);
+
+        String userInputText = edUserInput.getText().toString();
+
+        String selection = this.ddSelection.getSelectedItem().toString();
+        String resValue = getResources().getString(R.string.CharSelection);
+        Toast.makeText(this,String.valueOf(resValue), Toast.LENGTH_SHORT).show();
+
+        if (selection.equalsIgnoreCase(resValue)) {
+            int count = getCharsCount(userInputText);
+            tvOutput.setText(String.valueOf(count));
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/praktinisdarbas2_fintech2021/utils/ElementsCalculator.java
@@ -0,0 +1,10 @@
+package com.example.praktinisdarbas2_fintech2021.utils;
+public class ElementsCalculator {
+
+    public static int getCharsCount(String inputText) {
+        if (inputText != null) {
+            return inputText.length();
+        }
+        return 0;
+    }
+}
